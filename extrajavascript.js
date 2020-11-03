fca94fc0dcd06c20a15ca41c811e2ccb731e52ec
krockamichael <krocka.michael@gmail.com>
2020-11-03 19:13:12 +0100

    Update to visuals of "team meetings", "boss meetings", "sprints" items and their corresponding JS logic.

--- extrajavascript.js
+++ extrajavascript.js
@@ -14,9 +14,9 @@ function onlyAlone() {
       elements[i].style.display = "none";
   }
 
-  var sections_for_meetings_alone = 2
-  for (i = 1; i <= sections_for_meetings_alone; i++) {
-      let x = document.getElementById("meet_alone_" + i.toString());
+  var sections_for_team_meetings = 2
+  for (i = 1; i <= sections_for_team_meetings; i++) {
+      let x = document.getElementById("team_meet_" + i.toString());
       x.style.display = "block";
   }
 }
@@ -27,9 +27,9 @@ function onlyTogether() {
       elements[i].style.display = "none";
   }
 
-  var sections_for_meetings_together = 2
-  for (i = 1; i <= sections_for_meetings_together; i++) {
-      let x = document.getElementById("meet_together_" + i.toString());
+  var sections_for_boss_meetings = 2
+  for (i = 1; i <= sections_for_boss_meetings; i++) {
+      let x = document.getElementById("boss_meet_" + i.toString());
       x.style.display = "block";
   }
 }
